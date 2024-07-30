579b7d15a6ffad572b9ccea56549566f238658e1
DevNifemi <abioseimran@gmail.com>
2024-07-30 04:05:15 +0100

    built navigation menu section

---
+++ src/components/Sidebar/NavigationMenu.jsx
@@ -0,0 +1,50 @@
+import MenuLink from "./MenuLink";
+import arrow from "../../assets/icons/arrow-down.svg";
+import { IconButton } from "@mui/material";
+import { useState } from "react";
+
+const NavigationMenu = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleNavOpen = () => {
+    setOpen(!open);
+  };
+  return (
+    <div>
+      <div className="flex justify-between items-center px-5 mb-3">
+        <div className="text-[14px] text-[#778CA2] leading-[21px] font-light">
+          Navigation
+        </div>
+
+        <IconButton
+          onClick={handleNavOpen}
+          color="inherit"
+          aria-label="open nav"
+          edge="start"
+        >
+          <img
+            className={`transition-all duration-300 ${
+              open ? "rotate-0" : "rotate-[180deg]"
+            }`}
+            src={arrow}
+            alt=""
+          />
+        </IconButton>
+      </div>
+
+      <div
+        className={`flex overflow-hidden transition-[max-height] duration-300 ease-in-out flex-col items-center justify-center  ${
+          open ? "max-h-[1000px]" : "max-h-0"
+        }`}
+      >
+        <MenuLink active link={"Dashboard"} />
+        <MenuLink link={"Projects"} />
+        <MenuLink link={"Tasks"} />
+        <MenuLink link={"Calendar"} />
+        <MenuLink link={"Analytics"} />
+      </div>
+    </div>
+  );
+};
+
+export default NavigationMenu;
