cdee083e95e059ccb2d6d731e912adc0a62ce846
DevNifemi <abioseimran@gmail.com>
2024-07-30 04:40:26 +0100

    edit menu link to accept icons/emojis

--- src/components/Sidebar/MenuLink.jsx
+++ src/components/Sidebar/MenuLink.jsx
@@ -1,16 +1,21 @@
-const MenuLink = ({ link, href, active }) => {
+const MenuLink = ({ link, href, active, icon }) => {
   return (
     <div
-      className={`pl-[40px] h-[42px] w-[232px] flex items-center  ${
-        active ? "bg-white" : "hover:bg-slate-200/70"
+      className={`${
+        icon ? "pl-[18px]" : "pl-[35px]"
+      }  h-[42px] w-[232px] flex items-center  ${
+        active ? "bg-[#F8FAFB]" : "hover:bg-slate-200/70"
       }  rounded-lg cursor-pointer`}
     >
-      <div
-        className={`text-[14px] ${
-          active ? "text-[#4D7CFE]" : "text-[#252631]"
-        }  leading-[21px] font-semibold`}
-      >
-        {link}
+      <div className="flex gap-[10px] items-center">
+        <div>{icon && <div className="text-[20px]">{icon}</div>}</div>
+        <div
+          className={`text-[14px] ${
+            active ? "text-[#4D7CFE]" : "text-[#252631]"
+          }  leading-[21px] font-semibold`}
+        >
+          {link}
+        </div>
       </div>
     </div>
   );
