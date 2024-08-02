1c67ea9cee48f435d0a3448296c3a73e2dda84bc
DevNifemi <abioseimran@gmail.com>
2024-08-02 06:21:12 +0100

    created dashboard page

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,164 @@
+import { Checkbox, Chip, Grid, Paper, Stack } from "@mui/material";
+import arrow from "../assets/icons/blue-right-arrow.svg";
+import x from "../assets/icons/cancel.svg";
+
+import TodoListItem from "../components/Dashboard/TodoListItem";
+import Calendar from "../components/Dashboard/Calendar";
+import OverView from "../components/Dashboard/OverView";
+import PendingProjects from "../components/Dashboard/PendingProjects";
+
+const todos = [
+  { title: "Create non-annoying nudges", type: "Work", checked: true },
+  {
+    title: "Finalize API integration for new feature",
+    type: "Work",
+    checked: true,
+  },
+  { title: "Family Therapy at Austerlitz", type: "Family" },
+  { title: "Coordinate family dinner at the Summer Palace", type: "Family" },
+  { title: "Feed Mondale", type: "Home" },
+  { title: "iPad for kids’ rabbit", type: "Kids" },
+  { title: "PGN meeting at Tern Haven", type: "Work" },
+  { title: "Give staff day off (skeleton crew)", type: "Home" },
+  { title: "Buy birthday  gifts for Sophie and Iverson", type: "Family" },
+  { title: "Choose a successor", type: "Kids" },
+];
+
+const Dashboard = () => {
+  const handleDelete = () => {
+    console.info("You clicked the delete icon.");
+  };
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={8}>
+        <div className="flex items-center justify-between">
+          <div className="text-[16px] font-semibold leading-[24px] text-[#778CA2]">
+            Tasks overview
+          </div>
+
+          <div className="flex cursor-pointer hover:underline items-center font-semibold gap-2 text-[#4D7CFE] text-[14px] leading-[18px] pl-[30px]">
+            See all stats
+            <img src={arrow} alt="" />
+          </div>
+        </div>
+
+        <Paper
+          sx={{
+            boxShadow: "none",
+            border: "1px solid #E8ECEF",
+            borderRadius: "8px",
+            mt: 1,
+            mx: 0,
+            px: 0,
+          }}
+        >
+          <OverView />
+        </Paper>
+
+        <div className="flex items-center justify-between mt-4">
+          <Stack direction="row" alignItems="center" spacing={2}>
+            <div className="text-[16px] font-semibold leading-[24px] text-[#778CA2]">
+              All Tasks
+            </div>
+
+            <Stack direction="row" alignItems={"center"} spacing={1}>
+              <Chip
+                sx={{
+                  fontFamily: "Public Sans",
+                  color: "#778CA2",
+                  bgcolor: "#F2F4F6",
+                  height: "fit-content",
+                  p: "4px 14px 5px 16px !important",
+                }}
+                onDelete={handleDelete}
+                deleteIcon={<img src={x} />}
+                label="Complete"
+              />
+              <Chip
+                size="small"
+                sx={{
+                  fontFamily: "Public Sans",
+                  color: "#778CA2",
+                  bgcolor: "#F2F4F6",
+                  height: "fit-content",
+                  p: "3px 14px 4px 16px !important",
+                }}
+                label="Pending"
+              />
+              <Chip
+                size="small"
+                variant="outlined"
+                sx={{
+                  fontFamily: "Public Sans",
+                  color: "#778CA2",
+                  height: "fit-content",
+                  p: "3px 14px 4px 16px !important",
+                }}
+                label="Expired"
+              />
+            </Stack>
+          </Stack>
+
+          <div className="flex cursor-pointer hover:underline items-center  font-semibold gap-2 text-[#4D7CFE] text-[14px] leading-[18px] pl-[30px]">
+            See all tasks
+            <img src={arrow} alt="" />
+          </div>
+        </div>
+        <Paper
+          sx={{
+            boxShadow: "none",
+            border: "1px solid #E8ECEF",
+            borderRadius: "8px",
+            mt: 2,
+            mx: 0,
+            px: 0,
+          }}
+        >
+          <div className="h-[52px] mx-2 flex items-center justify-between border-b-[1px] border-[#E8ECEF]">
+            <div className="flex  items-center">
+              <Checkbox
+                sx={{
+                  "&.Mui-checked": {
+                    color: "#4D7CFE",
+                  },
+                }}
+              />
+              <div className="text-[14px] text-[#252631] leading-[21px] font-medium">
+                Type to create a new task …
+              </div>
+            </div>
+          </div>
+
+          {todos.map((todo, idx) => (
+            <TodoListItem
+              title={todo.title}
+              type={todo.type}
+              checked={todo?.checked}
+            />
+          ))}
+        </Paper>
+      </Grid>
+
+      <Grid item xs={4}>
+        <div className="text-[16px] font-semibold leading-[24px] text-[#778CA2] mb-2">
+          Calendar
+        </div>
+        <Paper
+          sx={{
+            boxShadow: "none",
+            border: "1px solid #E8ECEF",
+            p: 0,
+            m: 0,
+            borderRadius: "8px",
+          }}
+        >
+          <Calendar />
+        </Paper>
+
+        <PendingProjects />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Dashboard;
