2094b720f5892d747d56c88bf00e54d9c3296347
DevNifemi <abioseimran@gmail.com>
2024-08-02 06:26:07 +0100

    todo list item

---
+++ src/components/Dashboard/TodoListItem.jsx
@@ -0,0 +1,56 @@
+import Task from "../../assets/icons/task.svg";
+import Chat from "../../assets/icons/chat.svg";
+import { Checkbox } from "@mui/material";
+import Chip from "../Chip";
+import { useState } from "react";
+
+const TodoListItem = ({ title, type, checked = false }) => {
+  const [checkbox, setCheckbox] = useState(checked);
+
+  const handleChange = (event) => {
+    setCheckbox(event.target.checked);
+  };
+
+  return (
+    <div className="h-[52px] mx-2 flex items-center justify-between border-b-[1px] border-[#E8ECEF] cursor-pointer hover:bg-[#E8ECEF]/40">
+      <div className="flex  items-center">
+        <Checkbox
+          checked={checkbox}
+          onChange={handleChange}
+          sx={{
+            "&.Mui-checked": {
+              color: "#4D7CFE",
+            },
+          }}
+        />
+        <div
+          className={`text-[14px] ${
+            checkbox ? "text-[#778CA2]" : "text-[#252631] "
+          } leading-[21px] font-medium`}
+        >
+          {title}
+        </div>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="flex items-center gap-1">
+          <img className="w-[16px] h-[16px]" src={Task} alt="" />
+          <div className="text-[14px] text-[#778CA2] leading-[21px] font-normal">
+            12/28
+          </div>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <img className="w-[16px] h-[16px]" src={Chat} alt="" />
+          <div className="text-[14px] text-[#778CA2] leading-[21px] font-normal">
+            8
+          </div>
+        </div>
+
+        <Chip type={type} />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
