300b0824f9ba564e91589064a9abc847b1c2e942
DevNifemi <abioseimran@gmail.com>
2024-08-04 10:58:33 +0100

    complete analytics page

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -3,11 +3,16 @@ import React from "react";
 import Progress from "../components/Analytics/Progress";
 import graph from "../assets/analytics/people-data.webp";
 import graph2 from "../assets/analytics/task-graph.webp";
+import stats from "../assets/analytics/statistics.webp";
 
 const Analytics = () => {
   return (
     <section>
-      <div></div>
+      <div className="-mt-2 mb-2">
+        <img src={stats} alt="" />
+      </div>
+
+      <div className="bg-[#E8ECEF] h-[1px] mb-8 mt-8 w-screen -ml-[2vw]"></div>
 
       <Grid container spacing={2.5}>
         <Grid item xs={6}>
