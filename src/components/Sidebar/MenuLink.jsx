3bf5bf8aa1e499ce3ccfd172815c57877dcf0711
DevNifemi <abioseimran@gmail.com>
2024-08-05 08:55:52 +0100

    active menu should have border right

--- src/components/Sidebar/MenuLink.jsx
+++ src/components/Sidebar/MenuLink.jsx
@@ -4,25 +4,31 @@ const MenuLink = ({ link, href = "#", icon }) => {
   const match = useMatch(href);
 
   return (
-    <Link
-      to={href}
-      className={`${
-        icon ? "pl-[18px]" : "pl-[35px]"
-      }  h-[42px] w-[232px] flex items-center  ${
-        Boolean(match) ? "bg-[#F8FAFB]" : "hover:bg-slate-200/70"
-      }  rounded-lg cursor-pointer`}
+    <div
+      className={`w-full h-full ${
+        Boolean(match) && "border-r-[2px] border-[#4D7CFE]"
+      }  flex justify-center items-center`}
     >
-      <div className="flex gap-[10px] items-center">
-        <div>{icon && <div className="text-[20px]">{icon}</div>}</div>
-        <div
-          className={`text-[14px] ${
-            Boolean(match) ? "text-[#4D7CFE]" : "text-[#252631]"
-          }  leading-[21px] font-semibold`}
-        >
-          {link}
+      <Link
+        to={href}
+        className={`${
+          icon ? "pl-[18px]" : "pl-[35px]"
+        }  h-[42px] w-[232px] flex items-center  ${
+          Boolean(match) ? "bg-[#F8FAFB]" : "hover:bg-slate-200/70"
+        }  rounded-lg cursor-pointer`}
+      >
+        <div className="flex gap-[10px] items-center">
+          <div>{icon && <div className="text-[20px]">{icon}</div>}</div>
+          <div
+            className={`text-[14px] ${
+              Boolean(match) ? "text-[#4D7CFE]" : "text-[#252631]"
+            }  leading-[21px] font-semibold`}
+          >
+            {link}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
