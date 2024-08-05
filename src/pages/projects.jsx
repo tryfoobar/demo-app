0713f917889ff8875dbed92fca673a9a98999d95
DevNifemi <abioseimran@gmail.com>
2024-08-05 08:33:12 +0100

    create project page

---
+++ src/pages/projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectCard from "../components/Projects/ProjectCard";
+
+const Projects = () => {
+  return (
+    <section>
+      <div>
+        <ProjectCard />
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
