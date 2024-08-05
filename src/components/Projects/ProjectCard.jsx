9c194f82c0e1dadde8874d5e3d5f46b95fe03043
DevNifemi <abioseimran@gmail.com>
2024-08-05 18:03:49 +0100

    complete project card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import tool from "../../assets/icons/tool.svg";
 import users from "../../assets/people/three-user.webp";
 
-const ProjectCard = () => {
+const ProjectCard = ({ color, title, type, date, progress }) => {
   return (
     <div className="h-[336px] bg-white rounded-lg border border-[#E8ECEF] w-[363px] p-6 relative">
       <div className="flex justify-end">
@@ -9,20 +9,23 @@ const ProjectCard = () => {
       </div>
 
       <div className="flex flex-col items-center justify-center mt-4">
-        <div className="w-[74px] h-[74px] rounded-full bg-[#E9088A]"></div>
+        <div
+          style={{ background: color }}
+          className={`w-[74px] h-[74px] rounded-full`}
+        ></div>
         <div className="text-[16px] text-[#252631] leading-[24px] font-semibold mt-3">
-          40th Birthday Party
+          {title}
         </div>
 
         <div className="text-[14px] text-[#778CA2] leading-[21px] font-normal">
-          Due to: 21 July 2025
+          Due to: {date}
         </div>
 
         <div
           style={{ padding: "3px 14px 4px 16px" }}
           className="text-[14px] mt-6 text-[#778CA2] leading-[21px] font-normal flex items-center gap-2 rounded-[99px] bg-[#F2F4F6] cursor-pointer"
         >
-          Work
+          {type}
         </div>
 
         <img
@@ -33,7 +36,10 @@ const ProjectCard = () => {
       </div>
 
       <div className="h-[4px] bg-[#F2F4F6] left-0 w-full absolute bottom-0 rounded-lg">
-        <div className="bg-[#C2C8DC] w-[58%] h-full rounded-lg"></div>
+        <div
+          style={{ width: progress }}
+          className="bg-[#C2C8DC] h-full rounded-lg"
+        ></div>
       </div>
     </div>
   );
