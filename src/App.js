f6f7513a132e93cc7ae522d5578da50a80bb4558
DevNifemi <abioseimran@gmail.com>
2024-08-06 14:36:53 +0100

    netlify redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
 import DashboardLayout from "./layout/DashboardLayout";
-import { Outlet, useMatch, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const match = useMatch("/");
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (match?.pathname === "/") {
-      navigate("/dashboard");
-    }
-    // eslint-disable-next-line
-  }, []);
-
   return (
     <div className="">
       <DashboardLayout>
